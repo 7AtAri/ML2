b602dd8cfbd2d6b79dee68f4565f979494fca49c
7AtAri <7AtAri@users.noreply.github.com>
2023-12-06 22:29:40 +0100

    data imputation and report update

--- Setup.R
+++ Setup.R
@@ -6,8 +6,13 @@ library(inspectdf)
 library(corrplot)
 library(ggplot2)
 library(Hmisc)
-library(dplyr)
 library(naniar)
+library(mice)
+# library(missForest)
+library(dplyr)
+library(missRanger)
+library(skimr)
+
 
 # for example:
 # library(jsonlite)
@@ -32,72 +37,151 @@ rm(list = ls(all.names = TRUE))
 
 # ---- Data import ------------------------------------------------------------
 
-
 # import as dataframe
-RISK <- read_csv("datasets/National YRBS Datasets/XXHq.csv")
-
-RISK_qn <- read_csv("datasets/National YRBS Datasets/XXHqn.csv")
-
-RISK <- RISK %>% 
-  filter(!is.na(q25) & !is.na(q26) & !is.na(q27) & !is.na(q28) & !is.na(q29)) %>% 
-  mutate(suicidal_class = ifelse(!is.na(q29) & q29 > 1,   5,
-                          ifelse(!is.na(q28) & q28 > 1,   5,
-                          ifelse(!is.na(q27) & q27 == 1,  4,
-                          ifelse(!is.na(q26) & q26 == 1,  3,
-                          ifelse(!is.na(q25) & q25 == 1,  2,
-                                                          1))))))
-head(RISK)
-
-# breed_lab <- subset(read_csv("petdata/PetFinder-BreedLabels.csv",
-#                             show_col_types = FALSE), select = -c(Type))
-# color_lab <- read_csv("petdata/PetFinder-ColorLabels.csv",
-#                      show_col_types = FALSE)
-#state_lab <- read_csv("petdata/PetFinder-StateLabels.csv",
-#                      show_col_types = FALSE)
 
 
-# ---- Extract json data and dump it into the dataframe -----------------------
+RISK <- read_csv("datasets/National YRBS Datasets/XXHq.csv",show_col_types = FALSE)
+
+# RISK_qn <- read_csv("datasets/National YRBS Datasets/XXHqn.csv")
 
 
-# Path to the folder containing JSON files
-# folder_path <- "petdata/train_sentiment"
+# ---- Deleting redundant/unnecessary variables ------------------------------------------
 
-# Get a list of JSON file names in the folder
-# json_files <- list.files(folder_path, pattern = ".json$", full.names = TRUE)
+# all(RISK$site==RISK$site[1]) # proof: no information in site variable
+RISK$site<-NULL # only "XX"
+RISK$orig_rec<-NULL # only NAs
+RISK$q4<-NULL # aggregated in raceeth
+RISK$q5<-NULL # aggregated in raceeth
+RISK$record<-NULL # id column
 
-# Create an empty data frame with columns for "score" and "magnitude"
-# sentiment_df <- data.frame(
-#  SentimentScore = numeric(),
-#  SentimentMagnitude = numeric(),
-#  PetID = character(),
-#  stringsAsFactors = FALSE
-#)
+# RISK <- subset(RISK, select = c("site", "orig_rec", "q4","q5") ) # get rid of useless variables
 
-# Process each JSON file
-#for (json_file in json_files) {
-  # Read the JSON file as text
-#  json_text <- readLines(json_file, warn = FALSE)
-  # Parse the JSON text:
-#  json_data <- fromJSON(txt = json_text)
-  
-  # Extract document-level sentiment
-#  score <- json_data$documentSentiment$score
-#  magnitude <- json_data$documentSentiment$magnitude
-  
-  # Extract the file name:
-#  file_name <- basename(json_file)
-#  pattern <- str_extract(file_name, "[A-Za-z0-9]+")
-  
-  # Create a data frame for document-level sentiment
-#  df_new_row <- data.frame(SentimentScore = score,
-#                           SentimentMagnitude = magnitude,
-#                           PetID = pattern)
-#  sentiment_df <- rbind(sentiment_df, df_new_row)
-#}
+
+# head(RISK)
+
+# describe(RISK)
 
 
 
-# ---- Merge labels and add data to df ------------
+# ---- factorizing the categorical variables for imputation ----------------------------------------
+
+# the variables have to be factorized for imputation
+RISK$q1<-factor(RISK$q1,ordered = TRUE)
+RISK$raceeth<-factor(RISK$raceeth)
+RISK$q2<-factor(RISK$q2)
+RISK$q3<-factor(RISK$q3,ordered = TRUE)
+RISK$q8<-factor(RISK$q8,ordered = TRUE)
+RISK$q9<-factor(RISK$q9,ordered = TRUE)
+RISK$q10<-factor(RISK$q10,ordered = TRUE)
+RISK$q11<-factor(RISK$q11,ordered = TRUE)
+RISK$q12<-factor(RISK$q12,ordered = TRUE)
+RISK$q13<-factor(RISK$q13,ordered = TRUE)
+RISK$q14<-factor(RISK$q14,ordered = TRUE)
+RISK$q15<-factor(RISK$q15,ordered = TRUE)
+RISK$q16<-factor(RISK$q16,ordered = TRUE)
+RISK$q17<-factor(RISK$q17,ordered = TRUE)
+RISK$q18<-factor(RISK$q18)
+RISK$q19<-factor(RISK$q19)
+RISK$q20<-factor(RISK$q20,ordered = TRUE)
+RISK$q21<-factor(RISK$q21,ordered = TRUE)
+RISK$q22<-factor(RISK$q22,ordered = TRUE)
+RISK$q23<-factor(RISK$q23)
+RISK$q24<-factor(RISK$q24)
+RISK$q25<-factor(RISK$q25)
+RISK$q26<-factor(RISK$q26)
+RISK$q27<-factor(RISK$q27)
+RISK$q28<-factor(RISK$q28,ordered = TRUE)
+RISK$q29<-factor(RISK$q29)
+RISK$q30<-factor(RISK$q30)
+RISK$q31<-factor(RISK$q31,ordered = TRUE)
+RISK$q32<-factor(RISK$q32,ordered = TRUE)
+RISK$q33<-factor(RISK$q33,ordered = TRUE)
+RISK$q34<-factor(RISK$q34)
+RISK$q35<-factor(RISK$q35,ordered = TRUE)
+RISK$q36<-factor(RISK$q36)
+RISK$q37<-factor(RISK$q37,ordered = TRUE)
+RISK$q38<-factor(RISK$q38,ordered = TRUE)
+RISK$q39<-factor(RISK$q39)
+RISK$q40<-factor(RISK$q40,ordered = TRUE)
+RISK$q41<-factor(RISK$q41,ordered = TRUE)
+RISK$q42<-factor(RISK$q42,ordered = TRUE)
+RISK$q43<-factor(RISK$q43,ordered = TRUE)
+RISK$q44<-factor(RISK$q44)
+RISK$q45<-factor(RISK$q45,ordered = TRUE)
+RISK$q46<-factor(RISK$q46,ordered = TRUE)
+RISK$q47<-factor(RISK$q47,ordered = TRUE)
+RISK$q48<-factor(RISK$q48,ordered = TRUE)
+RISK$q49<-factor(RISK$q49,ordered = TRUE)
+RISK$q50<-factor(RISK$q50,ordered = TRUE)
+RISK$q51<-factor(RISK$q51,ordered = TRUE)
+RISK$q52<-factor(RISK$q52,ordered = TRUE)
+RISK$q53<-factor(RISK$q53,ordered = TRUE)
+RISK$q54<-factor(RISK$q54,ordered = TRUE)
+RISK$q55<-factor(RISK$q55)
+RISK$q56<-factor(RISK$q56)
+RISK$q57<-factor(RISK$q57)
+RISK$q58<-factor(RISK$q58,ordered = TRUE)
+RISK$q59<-factor(RISK$q59,ordered = TRUE)
+RISK$q60<-factor(RISK$q60,ordered = TRUE)
+RISK$q61<-factor(RISK$q61)
+RISK$q62<-factor(RISK$q62)
+RISK$q63<-factor(RISK$q63)
+RISK$q64<-factor(RISK$q64)
+RISK$q65<-factor(RISK$q65)
+RISK$q66<-factor(RISK$q66,ordered = TRUE)
+RISK$q67<-factor(RISK$q67)
+RISK$q68<-factor(RISK$q68,ordered = TRUE)
+RISK$q69<-factor(RISK$q69,ordered = TRUE)
+RISK$q70<-factor(RISK$q70,ordered = TRUE)
+RISK$q71<-factor(RISK$q71,ordered = TRUE)
+RISK$q72<-factor(RISK$q72,ordered = TRUE)
+RISK$q73<-factor(RISK$q73,ordered = TRUE)
+RISK$q74<-factor(RISK$q74,ordered = TRUE)
+RISK$q75<-factor(RISK$q75,ordered = TRUE)
+RISK$q76<-factor(RISK$q76,ordered = TRUE)
+RISK$q77<-factor(RISK$q77,ordered = TRUE)
+RISK$q78<-factor(RISK$q78,ordered = TRUE)
+RISK$q79<-factor(RISK$q79,ordered = TRUE)
+RISK$q80<-factor(RISK$q80,ordered = TRUE)
+RISK$q81<-factor(RISK$q81,ordered = TRUE)
+RISK$q82<-factor(RISK$q82)
+RISK$q83<-factor(RISK$q83)
+RISK$q84<-factor(RISK$q84,ordered = TRUE)
+RISK$q85<-factor(RISK$q85,ordered = TRUE)
+RISK$q86<-factor(RISK$q86,ordered = TRUE)
+RISK$q87<-factor(RISK$q87)
+RISK$q88<-factor(RISK$q88,ordered = TRUE)
+RISK$q89<-factor(RISK$q89,ordered = TRUE)
+RISK$q90<-factor(RISK$q90,ordered = TRUE)
+RISK$q91<-factor(RISK$q91,ordered = TRUE)
+RISK$q92<-factor(RISK$q92,ordered = TRUE)
+RISK$q93<-factor(RISK$q93,ordered = TRUE)
+RISK$q94<-factor(RISK$q94)
+RISK$q95<-factor(RISK$q95,ordered = TRUE)
+RISK$q96<-factor(RISK$q96,ordered = TRUE)
+RISK$q97<-factor(RISK$q97,ordered = TRUE)
+RISK$q98<-factor(RISK$q98)
+RISK$q99<-factor(RISK$q99, ordered= TRUE)
+RISK$stratum<-factor(RISK$stratum)
+RISK$psu<-factor(RISK$psu)
+RISK$q6orig<-factor(RISK$q6orig)
+RISK$q7orig<-factor(RISK$q7orig)
+
+
+# ---- creating the target variable -------
+# now is done in the main rmarkdown file.
+# this is the old version
+
+# RISK_imp <- RISK_imp %>% 
+#  filter(!is.na(q25) & !is.na(q26) & !is.na(q27) & !is.na(q28) & !is.na(q29)) %>% 
+#  mutate(suicidal_class = ifelse(!is.na(q29) & q29 > 1,   5,
+#                          ifelse(!is.na(q28) & q28 > 1,   5,
+#                          ifelse(!is.na(q27) & q27 == 1,  4,
+#                          ifelse(!is.na(q26) & q26 == 1,  3,
+#                          ifelse(!is.na(q25) & q25 == 1,  2,
+#                                                         1))))))
+
+# ---- Example: Merge labels and add data to df ------------
 
 
 # Add the sentiment score and maginitude to the petdata
@@ -110,23 +194,6 @@ head(RISK)
 # names(petdata)[names(petdata) == "Breed1"] <- "BreedID1"
 
 
-# ---- factorize categorical variables ----------------------------------------
-
-# examples:
-
-#petdata$SentimentScore_fac<-as.factor(petdata$SentimentScore)
-
-#petdata$SentimentScore_fac <- cut(petdata$SentimentScore,
-#                                  breaks = c(-1, -0.4, -0.1, 0.1, 0.4, 1),
-#                                  include.lowest = T,
-#                                  right = F)
-#levels(petdata$SentimentScore_fac)<-c("negative", "moderately negative", "neutral", "moderately positive", "positive")
-#petdata$ColorID1<-as.factor(petdata$ColorID1)
-
-
-# ---- Data preparation -------------------------------------------
-
-
 # ---- helper functions ------------------------------------------------------
 
 
@@ -136,5 +203,5 @@ head(RISK)
 # ---- Clean up the environment -----------------------------------------------
 
 
-# rm(...) remove unneeded variables after the setup
+# rm(...) remove unneeded (helper) variables after the setup
 
---
+++ names.R
@@ -0,0 +1,253 @@
+
+library(dplyr)
+
+
+## ------ naming the variables --------------
+
+if (!("age" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(age = q1)}
+
+if (!("sex" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(sex = q2)}
+
+if (!("grade" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(grade = q3)}
+
+if (!("height_feet" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(height_feet = q6)}
+
+if (!("weight_pounds" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(weight_pounds = q7)}
+
+if (!("seat_belt" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(seat_belt = q8)}
+
+if (!("drinking_driver_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(drinking_driver_30d = q9)}
+
+if (!("drinking_driving_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(drinking_driving_30d = q10)}
+
+if (!("texting_driving_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(texting_driving_30d = q11)}
+
+if (!("weapon_to_school_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(weapon_to_school_30d = q12)}
+
+if (!("gun_to_school_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(gun_to_school_12m = q13)}
+
+if (!("absence_safety_concerns_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(absence_safety_concerns_30d = q14)}
+
+if (!("weapon_threatened_at_school_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(weapon_threatened_at_school_12m = q15)}
+
+if (!("physical_fight_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(physical_fight_12m = q16)}
+
+if (!("physical_fight_at_school_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(physical_fight_at_school_12m = q17)}
+
+if (!("saw_attack" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(saw_attack = q18)}
+
+if (!("forced_sex" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(forced_sex = q19)}
+
+if (!("sexual_violence_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(sexual_violence_12m = q20)}
+
+if (!("sexual_dating_violence_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(sexual_dating_violence_12m = q21)}
+
+if (!("physical_dating_violence_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(physical_dating_violence_12m = q22)}
+
+if (!("bullied_at_school_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(bullied_at_school_12m = q23)}
+
+if (!("bullied_electronically_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(bullied_electronically_12m = q24)}
+
+if (!("tried_smoking" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(tried_smoking = q30)}
+
+if (!("smoking_age" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(smoking_age = q31)}
+
+if (!("smoking_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(smoking_30d = q32)}
+
+if (!("smoking_perday_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(smoking_perday_30d = q33)}
+
+if (!("vapor_tried" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(vapor_tried = q34)}
+
+if (!("vapor_use_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(vapor_use_30d = q35)}
+
+if (!("vapor_access" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(vapor_access = q36)}
+
+if (!("smokeless_tobacco_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(smokeless_tobacco_30d = q37)}
+
+if (!("cigar_use_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(cigar_use_30d = q38)}
+
+if (!("tried_stop_tobacco_12m" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(tried_stop_tobacco_12m = q39)}
+
+if (!("alcohol_age" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(alcohol_age = q40)}
+
+if (!("alcohol_use_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(alcohol_use_30d = q41)}
+
+if (!("alcohol_binging_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(alcohol_binging_30d = q42)}
+
+if (!("drinks_in_row_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(drinks_in_row_30d = q43)}
+
+if (!("alcohol_source_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(alcohol_source_30d = q44)}
+
+if (!("marijuana" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(marijuana = q45)}
+
+if (!("marijuana_age" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(marijuana_age = q46)}
+
+if (!("marijuana_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(marijuana_30d = q47)}
+
+if (!("marijuana_synth_30d" %in% colnames(RISK))) {
+  RISK <- RISK%>%
+    rename(marijuana_synth_30d= q48)}
+
+
+
+# ---------------naming the levels of the categorical variables -------------
+
+RISK$age<-factor(RISK$age,ordered = TRUE, labels= c("12orYounger","13","14","15","16","17","18orOlder"))
+
+RISK$raceeth<-factor(RISK$raceeth, labels= c("American/Alaskan Native","Asian", "Black or African American", "Native Pacific Islander", "White", "Hispanic/Latino", "Multiple-Hispanic/Latino", "Multiple-Non-Hispanic/Latino"))
+
+RISK$sex<-factor(RISK$sex, labels= c("female", "male"))
+
+RISK$grade<-factor(RISK$grade,ordered = TRUE, labels= c("9th", "10th", "11th", "12th", "undergraded or other"))
+
+RISK$seat_belt<-factor(RISK$seat_belt,ordered = TRUE, labels= c("Never", "Rarely", "Sometimes", "Mostly", "Always"))
+
+RISK$drinking_driver_30d<-factor(RISK$drinking_driver_30d,ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or more times"))
+
+RISK$drinking_driving_30d<-factor(RISK$drinking_driving_30d,ordered = TRUE, labels= c("did not drive", "0", "1", "2 or 3", "4 or 5", "6 or more times"))
+
+RISK$texting_driving_30d<-factor(RISK$texting_driving_30d,ordered = TRUE, labels= c("did not drive", "0", "1 or 2", "3 to 5", "6 to 9", "10 to 19", "20 to 29", "all 30 days"))
+
+RISK$weapon_to_school_30d<-factor(RISK$weapon_to_school_30d, ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or more days"))
+
+RISK$gun_to_school_12m<-factor(RISK$gun_to_school_12m, ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or more days"))
+
+RISK$absence_safety_concerns_30d<-factor(RISK$absence_safety_concerns_30d, ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or more days"))
+
+RISK$weapon_threatened_at_school_12m<-factor(RISK$weapon_threatened_at_school_12m, ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or 7", "8 or 9", "10 or 11", "12 or more times"))
+
+RISK$physical_fight_12m<-factor(RISK$physical_fight_12m, ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or 7", "8 or 9", "10 or 11", "12 or more times"))
+
+RISK$physical_fight_at_school_12m<-factor(RISK$physical_fight_at_school_12m, ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or 7", "8 or 9", "10 or 11", "12 or more times"))
+
+RISK$saw_attack<-factor(RISK$saw_attack, labels= c("Yes", "No"))
+
+RISK$forced_sex<-factor(RISK$forced_sex, labels= c("Yes", "No"))
+
+RISK$sexual_violence_12m<-factor(RISK$sexual_violence_12m,ordered = TRUE, labels= c("0", "1", "2 or 3", "4 or 5", "6 or more times"))
+
+RISK$sexual_dating_violence_12m<-factor(RISK$sexual_dating_violence_12m,ordered = TRUE, labels= c("did not date","0", "1", "2 or 3", "4 or 5", "6 or more times"))
+
+RISK$physical_dating_violence_12m<-factor(RISK$physical_dating_violence_12m,ordered = TRUE, labels= c("did not date","0", "1", "2 or 3", "4 or 5", "6 or more times"))
+
+RISK$bullied_at_school_12m<-factor(RISK$bullied_at_school_12m, labels= c("Yes", "No"))
+
+RISK$bullied_electronically_12m<-factor(RISK$bullied_electronically_12m, labels= c("Yes", "No"))
+
+RISK$tried_smoking<-factor(RISK$tried_smoking, labels= c("Yes", "No"))
+
+RISK$smoking_age<-factor(RISK$smoking_age,ordered = TRUE, labels= c("never tried","8 or younger", "9 or 10", "11 or 12", "13 or 14", "15 or 16", "17 or older"))
+
+RISK$smoking_30d<-factor(RISK$smoking_30d,ordered = TRUE, labels= c("0", "1 or 2", "3 to 5", "6 to 9", "10 to 19", "20 to 29", "all 30 days"))
+
+RISK$smoking_perday_30d<-factor(RISK$smoking_perday_30d,ordered = TRUE, labels= c("not smoked", "lass than 1", "1", "2 to 5","6 to 10", "10 to 20", "more than 20 cigarettes"))
+
+RISK$vapor_tried<-factor(RISK$vapor_tried, labels= c("Yes", "No"))
+
+RISK$vapor_use_30d<-factor(RISK$vapor_use_30d, ordered = TRUE, labels= c("0", "1 or 2", "3 to 5", "6 to 9", "10 to 19", "20 to 29", "all 30 days"))
+
+RISK$vapor_access<-factor(RISK$vapor_access, labels= c("not used", "friend or family", "vape/tobacco shop", "store", "kiosk", "internet", "store_stolen", "other"))
+
+RISK$smokeless_tobacco_30d<-factor(RISK$smokeless_tobacco_30d, ordered = TRUE,labels= c("0", "1 or 2", "3 to 5", "6 to 9", "10 to 19", "20 to 29", "all 30 days"))
+
+RISK$cigar_use_30d<-factor(RISK$cigar_use_30d, ordered = TRUE,labels= c("0", "1 or 2", "3 to 5", "6 to 9", "10 to 19", "20 to 29", "all 30 days"))
+
+RISK$tried_stop_tobacco_12m<-factor(RISK$tried_stop_tobacco_12m, labels= c("Did not use", "Yes", "No"))
+
+RISK$alcohol_age<-factor(RISK$alcohol_age,ordered = TRUE, labels= c("never tried","8 or younger", "9 or 10", "11 or 12", "13 or 14", "15 or 16", "17 or older"))
+
+RISK$alcohol_use_30d<-factor(RISK$alcohol_use_30d, ordered = TRUE,labels= c("0", "1 or 2", "3 to 5", "6 to 9", "10 to 19", "20 to 29", "all 30 days"))
+
+RISK$alcohol_binging_30d<-factor(RISK$alcohol_binging_30d,ordered = TRUE, labels= c("0",  "1", "2" , "3 to 5","6 to 9", "10 to 19", "20 or more days"))
+
+RISK$drinks_in_row_30d<-factor(RISK$drinks_in_row_30d,ordered = TRUE, labels= c("did not drink", "1 or 2", "3", "4", "5", "6 or 7", "8 or 9", "10 or more drinks"))
+
+RISK$alcohol_source_30d<-factor(RISK$alcohol_source_30d, labels= c("no drink", "store","restaurant/bar/club", "public event","gave_someone_money", "someone gave me", "store/family_stolen",  "other"))
+
+RISK$marijuana<-factor(RISK$marijuana,ordered = TRUE, labels= c("0",  "1 or 2" , "3 to 9", "10 to 19", "20 to 39", "40 to 99", "100 times or more"))
+
+RISK$marijuana_age<-factor(RISK$marijuana_age,ordered = TRUE, labels= c("never tried","8 or younger", "9 or 10", "11 or 12", "13 or 14", "15 or 16", "17 or older"))
+
+RISK$marijuana_30d<-factor(RISK$marijuana_30d,ordered = TRUE, labels= c("0",  "1 or 2" , "3 to 9", "10 to 19", "20 to 39", "40 times or more"))
+
+RISK$marijuana_synth_30d<-factor(RISK$marijuana_synth_30d,ordered = TRUE, labels= c("0",  "1 or 2" , "3 to 9", "10 to 19", "20 to 39", "40 times or more"))
